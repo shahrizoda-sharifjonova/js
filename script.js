20badc0e41e889b5d6683cb8e12eb89d3621a7c7
Shahrizoda <shahrizodadeveloping@gmail.com>
2021-08-22 11:12:14 +0500

    initial

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// 1-Переменные
-// camelCase
-const firstName = "Vladilen"
-const lastName = "Minin" //string
-let age = 26 //number
-const isProgrammer = true //boolean
-
-var x = 1,
-    y = 356,
-    z = 37;
-
-console.log (x + y + z)
-
-
 
 
 
